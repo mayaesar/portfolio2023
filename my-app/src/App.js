47a7df932cb5ccfa401cf81af676f11137dabc95
Maya <48359706+mayaesar@users.noreply.github.com>
2023-02-07 14:16:34 -0500

    started structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+import { Device } from "./Devices";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyles />
+      <div className="image">
+
+      </div>
+      <div className="typing">
+        
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 90%;
+  margin: auto;
+  padding-top: 40px;
+  .image{
+    border: 1px solid black;
+    height: 200px;
+    width: 200px;
+    margin: auto;
+  }
+`;
 
 export default App;
---
+++ my-app/src/sections/About.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Device } from "./Devices";
+
+
+const About = () => {
+    return (
+        <Wrapper>
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+
+`;
+
+export default About;
